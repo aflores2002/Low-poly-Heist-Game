4a252dcc4afb9f7be28ee2bb140a767eaed449ba
aguerzoni <145509955+Artuuuu8@users.noreply.github.com>
2025-05-12 12:33:59 -0700

    Added walking npc to opposite sidewalk+ sitting npc on bench

---
+++ Assets/Scenes/Artu/CivialianWander.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CivilianWander : MonoBehaviour {
+    public Transform[] waypoints;
+    private NavMeshAgent agent;
+    private int currentTarget;
+    private bool isFleeing = false;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        ChooseNextDestination();
+    }
+
+    void Update() {
+        if (isFleeing) return;
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f) {
+            ChooseNextDestination();
+        }
+    }
+
+    void ChooseNextDestination() {
+        if (waypoints.Length == 0) return;
+
+        currentTarget = Random.Range(0, waypoints.Length);
+        agent.SetDestination(waypoints[currentTarget].position);
+    }
+
+    public void TriggerFlee(Transform fleeTarget) {
+        isFleeing = true;
+        agent.SetDestination(fleeTarget.position);
+    }
+}
+
